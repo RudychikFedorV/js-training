1db9cf890940c65ae10c4372a9ed03ae7008d5b3
RudychikFedorV <rudychik.fedor@gmail.com>
2023-12-23 19:38:46 +0200

    commit

---
+++ main.js
@@ -0,0 +1,57 @@
+const myName = "Fedir";
+const myCity = "Zmiev";
+
+const myFn = `My name is ${myName}. I live in the city ${myCity}.`;
+console,console.log(myFn);
+
+
+// function myFn(a, b) {
+//   let c;
+//   a = a + 1;
+//   c = a + b;
+//   return c;
+// }
+// console.dir(myFn);
+
+
+// const myCity = {
+//   city: "New York",
+//   cityGreeting() {
+//     console.log('Greetings!!!')
+//   }
+// }
+// myCity.cityGreeting()
+
+
+// const myCity = {
+//   city: "New York",
+// };
+// myCity.city = 'Las Vegas'
+// console.log(myCity)
+
+
+// const myCity = {
+//     city: 'New York'
+// }
+// myCity.popular = true
+// myCity.country = 'USA'
+// console.log(myCity)
+
+
+// let a = 10
+// console.log(a)
+// a = true
+// console.log(a)
+// a = 'Fedor'
+// console.log(a)
+// a = undefined
+// console.log(a)
+
+
+// const objectA = {
+//     a: 10,
+//     b: true
+// }
+// const copyOfA = objectA
+// copyOfA.c = 'abc'
+// console.log(objectA)
